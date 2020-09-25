468d81d1f85162fd442a64ffd39d577f5940a4d2
mattimm <z5187551@student.unsw.edu.au>
2020-09-25 18:18:06 +1000

    setup concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
+import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const testToSeeIfItWorks = (
+const testToSeeIfItWorks = () => (
   <div className='App'>
     <header className='App-header'>
       <img src={logo} className='App-logo' alt='logo' />
