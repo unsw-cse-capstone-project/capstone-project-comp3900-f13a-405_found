b3395330d516fc9cb4611a555bae48d9e091251b
mattimm <z5187551@student.unsw.edu.au>
2020-09-25 17:40:28 +1000

    Setup Backend

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const connectToMongoDB = require("./config/db");
+const app = express();
+// prevent mongodb injections
+const mongoSanitize = require("express-mongo-sanitize");
+
+connectToMongoDB();
+
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// Routes
+app.use("/api/authentication", require("./routes/api/authentication"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
