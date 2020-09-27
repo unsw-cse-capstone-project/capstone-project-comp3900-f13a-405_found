869631e01bbb9e06c2bf72ebcaa25405f151be2f
mattimm <z5187551@student.unsw.edu.au>
2020-09-28 08:45:35 +1000

    changed the styling a bit

--- client/src/components/alert/Alert.jsx
+++ client/src/components/alert/Alert.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Alert.scss";
 import { removeAlert } from "../../actions/alert";
-
+import AlertBootstrap from "react-bootstrap/Alert";
 const Alert = () => {
   const alertState = useSelector((state) => state.alert);
   const dispatch = useDispatch();
@@ -10,7 +10,22 @@ const Alert = () => {
     alertState !== null &&
     alertState.length > 0 &&
     alertState.map((alert) => (
-      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      <AlertBootstrap
+        variant={alert.alertType}
+        onClose={() => {
+          dispatch(removeAlert(alert.id));
+        }}
+        dismissible
+      >
+        <AlertBootstrap.Heading>Oh snap!</AlertBootstrap.Heading>
+        <p>{alert.msg}</p>
+      </AlertBootstrap>
+    ))
+  );
+};
+
+export default Alert;
+/* <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
         <div
           className='cross-button'
@@ -20,9 +35,4 @@ const Alert = () => {
         >
           X
         </div>
-      </div>
-    ))
-  );
-};
-
-export default Alert;
+</div> */
