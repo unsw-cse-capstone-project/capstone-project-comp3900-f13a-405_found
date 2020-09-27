645bd538ef9b349418ae9b49f6708c985396fc07
mattimm <z5187551@student.unsw.edu.au>
2020-09-28 01:35:57 +1000

    react complaining about unique id lol

--- client/src/components/alert/Alert.jsx
+++ client/src/components/alert/Alert.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import "./Alert.scss";
 import { removeAlert } from "../../actions/alert";
 import Container from "react-bootstrap/Container";
-
 const Alert = () => {
   const alertState = useSelector((state) => state.alert);
   const dispatch = useDispatch();
@@ -11,8 +10,8 @@ const Alert = () => {
     alertState !== null &&
     alertState.length > 0 &&
     alertState.map((alert) => (
-      <Container>
-        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      <Container key={alert.id}>
+        <div className={`alert alert-${alert.alertType}`}>
           {alert.msg}
           <div
             className='cross-button'
