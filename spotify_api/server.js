61db526d4b553a02f3ebd84f5e16e900b7e0f9b6
Yushin Kang <yushink@c1msm2vxh3qk.lan>
2020-09-30 18:20:38 +1000

    initial spotify-api

---
+++ spotify_api/server.js
@@ -0,0 +1,14 @@
+/* Load the HTTP library */
+var http = require("http");
+
+
+
+/* Create a HTTP server to handle responses */
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);
+
+
+
