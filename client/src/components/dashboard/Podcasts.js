fe42504920eb48c72f05fd0b085a2c7ff72243be
tassja <xtassja@gmail.com>
2020-10-16 01:09:12 +1100

    fix pic crash

--- client/src/components/dashboard/Podcasts.js
+++ client/src/components/dashboard/Podcasts.js
@@ -10,9 +10,7 @@ const Podcasts = ({ podcasts, loading }) => {
       {podcasts.map(podcasts => (
         <li key={podcasts.id} className='list-group-item'>
           <div style={{textAlign: 'left', color:'black'}}>{podcasts.name}  </div>
-        <div style={{textAlign: 'right', color:'black'}}> Subscriber count: {Math.floor(Math.random() * 115) + 1}k <img src={podcasts.images[2].url} /></div>
-          
-         
+          <div style={{textAlign: 'right', color:'black'}}> Subscriber count: {Math.floor(Math.random() * 115) + 1}k <img height="60px" width="60px"src={podcasts.images[0].url} /></div>
         </li>
       ))}
     </ul>
