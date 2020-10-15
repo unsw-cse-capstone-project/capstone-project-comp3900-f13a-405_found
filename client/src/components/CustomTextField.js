63c8aa223cdbfba5b568644bd2178412df9ea012
mattimm <z5187551@student.unsw.edu.au>
2020-10-15 14:07:23 +1100

    init

--- client/src/components/CustomTextField.js
+++ client/src/components/CustomTextField.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 export default function CustomTextField(props) {
-  const { name, type = "text", label, value, error = null, onChange } = props;
-  return (
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    name,
+    type = "text",
+    label,
+    value,
+    error = null,
+    onChange,
+    passwordViewable = false,
+  } = props;
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return !passwordViewable ? (
     <TextField
       variant='outlined'
       label={label}
@@ -14,5 +41,34 @@ export default function CustomTextField(props) {
       autoComplete='off'
       {...(error && { error: true, helperText: error })}
     />
+  ) : (
+    <FormControl
+      variant='outlined'
+      error={error && error.length > 0 ? true : false}
+    >
+      <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
+      <OutlinedInput
+        id='outlined-adornment-password'
+        type={showPassword ? "text" : "password"}
+        name='password'
+        value={value}
+        onChange={onChange}
+        {...(error && { error: true })}
+        endAdornment={
+          <InputAdornment position='end'>
+            <IconButton
+              aria-label='toggle password visibility'
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge='end'
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
+        labelWidth={70}
+      />
+      <FormHelperText id='component-error-text'>{error}</FormHelperText>
+    </FormControl>
   );
 }
--- client/src/components/authentication/Signup.jsx
+++ client/src/components/authentication/Signup.jsx
@@ -4,69 +4,77 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import "./Signup.css";
 
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
 import logo from "../landing/logo.png";
-import Typography from '@material-ui/core/Typography';
-import  CustomTextField from "../CustomTextField"
+import Typography from "@material-ui/core/Typography";
+import CustomTextField from "../CustomTextField";
 
 const Signup = () => {
-
-  const initialFValues= {
+  const initialFValues = {
     id: 0,
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-}
-  
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
   const authenticationState = useSelector((state) => state.authentication);
   const [values, setValues] = useState(initialFValues);
   const [errors, setErrors] = useState({});
   const dispatch = useDispatch();
-  
-const handleSubmit = e => {
-  e.preventDefault()
-  if (validate()){
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
       const { id, name, email, password } = values;
       dispatch(signup({ name, email, password }));
       //resetForm()
-  }
-}
+    }
+  };
 
-const handleInputChange = e => {
-  const { name, value } = e.target
-  setValues({
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    console.log(values);
+    setValues({
       ...values,
-      [name]: value
-  })
-  //if (validateOnChange)
-      validate({ [name]: value })
-}
+      [name]: value,
+    });
+    //if (validateOnChange)
+    validate({ [name]: value });
+  };
 
-const resetForm = () => {
-  setValues(initialFValues);
-  setErrors({})
-}
+  const resetForm = () => {
+    setValues(initialFValues);
+    setErrors({});
+  };
 
-const validate = (fieldValues = values) => {
-  let temp = { ...errors }
-  if ('name' in fieldValues)
-      temp.name = fieldValues.name.length < 4 ? "At least 4 characters required" : ""
-  if ('email' in fieldValues)
-      temp.email = (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/).test(fieldValues.email) ? "" : "Email address is invalid."
-  if ('password' in fieldValues)
-      temp.password = fieldValues.password.length < 6 ? "At least 6 characters required" : ""
-  if ('confirmPassword' in fieldValues)
-  temp.confirmPassword = fieldValues.confirmPassword !== values.password ? "Passwords do not match!" : ""
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors };
+    if ("name" in fieldValues)
+      temp.name =
+        fieldValues.name.length < 4 ? "At least 4 characters required" : "";
+    if ("email" in fieldValues)
+      temp.email = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(
+        fieldValues.email
+      )
+        ? ""
+        : "Email address is invalid.";
+    if ("password" in fieldValues)
+      temp.password =
+        fieldValues.password.length < 6 ? "At least 6 characters required" : "";
+    if ("confirmPassword" in fieldValues)
+      temp.confirmPassword =
+        fieldValues.confirmPassword !== values.password
+          ? "Passwords do not match!"
+          : "";
 
-  setErrors({
-      ...temp
-  })
+    setErrors({
+      ...temp,
+    });
 
-  if (fieldValues === values)
-      return Object.values(temp).every(x => x === "")
-}
-
+    if (fieldValues === values)
+      return Object.values(temp).every((x) => x === "");
+  };
 
   // redirect to dashboard if the user is authenticated
   if (authenticationState.isLoaded && authenticationState.isAuthenticated) {
@@ -74,59 +82,62 @@ const validate = (fieldValues = values) => {
   }
   return (
     <form onSubmit={handleSubmit}>
-            <div className="row">
-          <div className="middle-column2">
+      <div className='row'>
+        <div className='middle-column2'>
           <img src={logo} className='App-logo' alt='logo' />
-          <Typography variant="h5" gutterBottom> Sign up for UltraCast </Typography>
-                    <CustomTextField
-                        name="name"
-                        label="Full Name"
-                        variant="outlined"
-                        
-                        value={values.name}
-                        onChange={handleInputChange}
-                        error={errors.name}
-                        //helperText={errors.name}
-                        
-                    /><p></p>
-                    <CustomTextField
-                        label="Email"
-                        name="email"
-                        variant="outlined"
-                        
-                        value={values.email}
-                        onChange={handleInputChange}
-                        error={errors.email}
-                    /><p></p>
-                    <CustomTextField
-                        label="Password"
-                        name="password"
-                        variant="outlined"
-                        
-                        value={values.password}
-                        onChange={handleInputChange}
-                        error={errors.password}
-                    /><p></p>
-                    <CustomTextField
-                        variant="outlined"
-                        label="Confirm Password"
-                        name="confirmPassword"
-                        
-                        value={values.confirmPassword}
-                        onChange={handleInputChange}
-                        error={errors.confirmPassword}
-                    /><p></p>
-                    <Button variant="contained"
-                            type="submit">
-                                Submit</Button>
-                        <Button variant="contained"
-                            onClick={resetForm} > Reset</Button>
-                    </div>
-                    
-          </div>
-                    <div>            
-                    </div>
-        </form>
+          <Typography variant='h5' gutterBottom>
+            {" "}
+            Sign up for UltraCast{" "}
+          </Typography>
+          <CustomTextField
+            name='name'
+            label='Full Name'
+            variant='outlined'
+            value={values.name}
+            onChange={handleInputChange}
+            error={errors.name}
+            //helperText={errors.name}
+          />
+          <p></p>
+          <CustomTextField
+            label='Email'
+            name='email'
+            variant='outlined'
+            value={values.email}
+            onChange={handleInputChange}
+            error={errors.email}
+          />
+          <p></p>
+          <CustomTextField
+            label='Password'
+            name='password'
+            variant='outlined'
+            value={values.password}
+            onChange={handleInputChange}
+            error={errors.password}
+            passwordViewable={true}
+          />
+          <p></p>
+          <CustomTextField
+            variant='outlined'
+            label='Confirm Password'
+            name='confirmPassword'
+            value={values.confirmPassword}
+            onChange={handleInputChange}
+            error={errors.confirmPassword}
+          />
+          <p></p>
+          <Button variant='contained' type='submit'>
+            Submit
+          </Button>
+          <Button variant='contained' onClick={resetForm}>
+            {" "}
+            Reset
+          </Button>
+        </div>
+      </div>
+      <div></div>
+    </form>
   );
 };
 
