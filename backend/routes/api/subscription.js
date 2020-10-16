b7072216de1c19779af23fc82d8b77b80309baac
mattimm <z5187551@student.unsw.edu.au>
2020-10-16 18:18:44 +1100

    Subscribe endpoint done

---
+++ backend/routes/api/subscription.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require("axios");
+const { BadRequest } = require("../../utils/errors");
+const router = express.Router();
+const Subscription = require("../../models/SubscriptionModel");
+
+// @route   POST api/subscription/subscribe/:showId
+// @desc    Subscribe to a show
+// @access  Private
+router.post("/subscribe/:showId", async (req, res, next) => {
+  try {
+    // fetching this first will ensure that the provided showId is legit
+    const uri = encodeURI(
+      `https://api.spotify.com/v1/shows/${req.params.showId}/episodes?market=AU`
+    );
+    const headers = {
+      "user-agent": "node.js",
+      Authorization: `Bearer ${SPOTIFY_ACCESS_TOKEN}`,
+    };
+    const spotifyResponse = await axios.get(uri, { headers });
+
+    const showEpisodesId = spotifyResponse.data.items.map(
+      (episode) => episode.id
+    );
+
+    let subscribe = await Subscription.findOneAndUpdate(
+      {
+        user: req.user.id,
+        showId: req.params.showId,
+      },
+      { showId: req.params.showId },
+      {
+        new: true,
+        upsert: true,
+      }
+    );
+
+    subscribe.showEpisodesIds = showEpisodesId;
+    await subscribe.save();
+
+    return res.status(200).json(subscribe);
+  } catch (err) {
+    console.error(err.message);
+    next(new BadRequest([{ msg: "Bad Request!!" }]));
+  }
+});
+
+module.exports = router;
