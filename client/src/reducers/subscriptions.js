d1671a3ec90e550a9647a5ec66a1ec134dedb307
mattimm <z5187551@student.unsw.edu.au>
2020-10-16 19:57:30 +1100

    reducers done!!!!!!!!!!!!!!

---
+++ client/src/reducers/subscriptions.js
@@ -0,0 +1,37 @@
+import {
+  SUBSCRIBE_SUCCESS,
+  UNSUBSCRIBE_SUCCESS,
+  GET_SUBSCRIPTIONS,
+} from "../actions/types";
+
+const initialState = {
+  isLoaded: false,
+  subscriptions: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SUBSCRIBE_SUCCESS:
+      return {
+        ...state,
+        subscriptions: state.subscriptions.push(action.payload),
+        isLoaded: true,
+      };
+    case UNSUBSCRIBE_SUCCESS:
+      return {
+        ...state,
+        isLoaded: true,
+        subscriptions: state.subscriptions.filter(
+          (subs) => subs != action.payload
+        ),
+      };
+    case GET_SUBSCRIPTIONS:
+      return {
+        ...state,
+        isLoaded: true,
+        subscriptions: action.payload.map((result) => result.showId),
+      };
+    default:
+      return state;
+  }
+}
