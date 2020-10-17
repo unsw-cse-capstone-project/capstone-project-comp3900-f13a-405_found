337b097b550f19a032b5b4fe5806f7091a93f12f
tassja <xtassja@gmail.com>
2020-10-18 01:38:50 +1100

    clean

--- client/src/components/dashboard/Podcasts.js
+++ client/src/components/dashboard/Podcasts.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import SubscribeButton from "../SubscribeButton";
-
-
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
   root: {
@@ -47,22 +41,13 @@ const DialogContent = withStyles((theme) => ({
   },
 }))(MuiDialogContent);
 
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-
 const Podcasts = ({ podcasts, loading }) => {
 
   const [open, setOpen] = useState(false);
-  const theme = useTheme();
   const [podcast, setPodcast] = useState({});
 
-  const handleClickOpen = (term) => {
-    setPodcast(term);
+  const handleClickOpen = (pod) => {
+    setPodcast(pod);
     setOpen(!open);
 
   };
@@ -72,17 +57,14 @@ const Podcasts = ({ podcasts, loading }) => {
 
   if (loading) {
     return <h2>Loading...</h2>;
-  } 
-
-//onItemClick.bind(this, podcasts.id)
+  }
 
   return (
     <ul  className='list-group mb-4'>
       {podcasts.map((podcasts) => (
-        
         <li onClick={handleClickOpen.bind(this, podcasts)} key={podcasts.id} className='list-group-item'>
           <div style={{ textAlign: "left", color: "black" }}>
-            {podcasts.name}{" "}
+            {podcasts.name}
           </div>
           <div style={{ textAlign: "right", color: "black" }}>
             <img
@@ -91,7 +73,6 @@ const Podcasts = ({ podcasts, loading }) => {
               src={podcasts.images[0].url}
               alt='podcastimage'
             />
-            
           </div>
         </li>
       ))}
@@ -102,18 +83,14 @@ const Podcasts = ({ podcasts, loading }) => {
         </DialogTitle>
         <DialogContent dividers>
           <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            {podcast.description}
           </Typography>
           <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor.
           </Typography>
           <Typography gutterBottom>
           <SubscribeButton id={podcast.id} />
           </Typography>
-        </DialogContent>
-       
+        </DialogContent>       
       </Dialog>
     </ul>
   );
