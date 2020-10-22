0c8e7979e594254f9e8bb4298dfbbe94be01b8a5
tsoyuzhu <z5113384@student.unsw.edu.au>
2020-10-22 17:32:38 +1100

    modified styling logic on notifications so it doesn't reset on each click

--- client/src/components/notifications/notificationBox.js
+++ client/src/components/notifications/notificationBox.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const NotificationBox = (props) => {
    const notifications = props.notifications;
     return (
-        <div>
+        <div style={{display: props.expanded ? 'block' : 'none'}}>
             {   notifications.length == 0 ? 
                 <p>No new notifications</p>
                 : notifications.map( (notification,i) => <NotificationCard key={i} {...notification} />)
