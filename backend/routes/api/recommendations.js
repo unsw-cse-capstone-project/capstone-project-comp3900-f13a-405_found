f79a00fba1b72052a0f3b6d4365d5605af3b4e7f
Andrew Gallagher <z5118026@student.unsw.edu.au>
2020-11-02 23:08:56 +1100

    Added functionality for new users (no history and subscriptions)

--- backend/routes/api/recommendations.js
+++ backend/routes/api/recommendations.js
@@ -11,23 +11,22 @@ const { BadRequest } = require("../../utils/errors");
 const router = express.Router();
 
 // @route  GET api/recommendations
-// @desc   For the logged in user, returns a playlist containing user recommendations (spotify id, name, artwork). 
+// @desc   For the logged in user, returns an recommendations object with properties nrecs = no recs and recs = [{o.type = spotify show}, ... {...}]
 // @access Private
 
 router.get('/', async(req, res, next) => {
     try {
         var recommendations = {};
         const userRecord = new UserRecord();
-        const subscriptions = await userRecord.getUserSubscription(req); // array of objects containing only showIds (JSON string formatted).
-        const history = await userRecord.getUserHistory(req); // array of objects containing only episodeIds (JSON string formatted).
+        const subscriptions = await userRecord.getUserSubscription(req); 
+        const history = await userRecord.getUserHistory(req);
         if (subscriptions.length != 0 && history.length != 0) {
 
             const episodeMatch = new EpisodeMatch();
             const spotifyShowIds = await episodeMatch.getShowIds(history, subscriptions);
 
             const searchParams = new SearchParams();
-            const showsList = await searchParams.getSearchParams(spotifyShowIds); // returns an array of objects [{title, publisher}, ..., {t_n, p_n}]
-
+            const showsList = await searchParams.getSearchParams(spotifyShowIds); 
             const listenNotes = new ListenNotes();
             const listenNotesRecs = await listenNotes.getListenNotesRecs(showsList);
 
