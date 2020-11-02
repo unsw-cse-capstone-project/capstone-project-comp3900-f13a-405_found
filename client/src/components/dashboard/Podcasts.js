4cba1f7a8aa13eee3963fde253f0d3fde9a6bfdc
tsoyuzhu <z5113384@student.unsw.edu.au>
2020-11-02 18:42:39 +1100

    accidetally merged broken code. fixing it

--- client/src/components/dashboard/Podcasts.js
+++ client/src/components/dashboard/Podcasts.js
@@ -9,11 +9,7 @@ import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
 import axios from "axios";
 import Episodes from "./Episodes";
-<<<<<<< HEAD
-import PlaylistSelector from "./PlaylistSelector"
-=======
 import { DetailedView } from "./DetailedView"
->>>>>>> develop
 
 const styles = (theme) => ({
   root: {
